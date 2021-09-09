7197a2b5fc09a676602d9d8a839bbfed7ac5ef05
Mateusz Krystkowicz <krissie515@gmail.com>
2021-09-09 13:50:03 +0200

    add for hire label

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,14 +1,28 @@
 import React from "react"
+import PropTypes from "prop-types"
 import Avatar from "../Avatar/Avatar"
 import classes from "./UserInfo.module.scss"
 
-const UserInfo = () => {
+const UserInfo = ({ username, hireable, avatarUrl }) => {
+  let usernameText
+
+  if (hireable) {
+    usernameText = <p className={[classes.Username, classes.ForHire].join(" ")}>{username}</p>
+  } else {
+    usernameText = <p className={classes.Username}>{username}</p>
+  }
+
   return (
     <div className={classes.UserInfo}>
-      <Avatar />
-      <p className={classes.Username}>mkrystkowicz</p>
+      <Avatar avatarUrl={avatarUrl} username={username} />
+      {usernameText}
     </div>
   )
+}
+UserInfo.propTypes = {
+  username: PropTypes.string.isRequired,
+  hireable: PropTypes.bool.isRequired,
+  avatarUrl: PropTypes.string.isRequired,
 }
 
 export default UserInfo
